074ac1422e73902c77dc7620593d9a0419811b8d
shihaoyan003 <shihaoyan003@ke.com>
2024-10-06 15:08:34 +0800

    init

---
+++ shy-data-structure-web/src/test/java/com/shy/shy/data/structure/ShyDataStructureWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shy.shy.data.structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShyDataStructureWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
