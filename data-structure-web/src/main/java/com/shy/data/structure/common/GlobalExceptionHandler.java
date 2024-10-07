0a89f7e666a11231dea0cba823a1a912ef0299a2
shihaoyan <shihaoyan0209@163.com>
2024-10-07 19:50:44 +0800

    feat:处理全局异常

--- data-structure-web/src/main/java/com/shy/data/structure/common/GlobalExceptionHandler.java
+++ data-structure-web/src/main/java/com/shy/data/structure/common/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.shy.data.structure.common;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -12,7 +15,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Throwable.class)
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public Result<String> handleException(Exception e) {
         return Result.fail(e.getMessage());
     }
