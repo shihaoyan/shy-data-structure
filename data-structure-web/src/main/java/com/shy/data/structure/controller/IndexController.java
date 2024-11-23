e82a2f09c42860e3671ea3b093a9ee917b4b6fc5
shihaoyan <shihaoyan0209@163.com>
2024-11-23 21:52:16 +0800

    feat:增加健康检查路径

---
+++ data-structure-web/src/main/java/com/shy/data/structure/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.shy.data.structure.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "ok";
+    }
+
+
+
+
+}
